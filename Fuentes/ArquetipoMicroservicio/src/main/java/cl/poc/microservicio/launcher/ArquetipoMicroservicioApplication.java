20880166c026306069c6e088f5edb967352e612b
Camilo Contreras <contreras.camilo32@gmail.com>
2019-09-11 17:54:44 -0300

    carga inicial

---
+++ Fuentes/ArquetipoMicroservicio/src/main/java/cl/poc/microservicio/launcher/ArquetipoMicroservicioApplication.java
@@ -0,0 +1,19 @@
+package cl.poc.microservicio.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Punto de inicio para la aplicacion
+ * 
+ * @author ccontrerasc
+ *
+ */
+@SpringBootApplication
+public class ArquetipoMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquetipoMicroservicioApplication.class, args);
+	}
+
+}
